53fdb6973193cdd765d5b47c4da0596cfa714a20
angelo <angelo@fluxton.co.uk>
2019-07-15 01:04:59 +0100

    that will do

--- pura.zsh
+++ pura.zsh
@@ -532,7 +532,7 @@ prompt_pure_state_setup() {
 }
 
 
-prompt_pure_setup() {
+prompt_pura_setup() {
 	# Prevent percentage showing up if output doesn't end with a newline.
 	export PROMPT_EOL_MARK=''
 
@@ -624,5 +624,5 @@ prompt_pure_setup() {
 	unset ZSH_THEME
 }
 
-prompt_pure_setup "$@"
+prompt_pura_setup "$@"
 
